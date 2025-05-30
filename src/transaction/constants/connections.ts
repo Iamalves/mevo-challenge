56ec0cb8168b754a5ea91b3759caee78a8c721c6
bruno alves <balvesocial@gmail.com>
2025-05-30 11:08:38 -0300

    feat: ajustando envs

--- src/transaction/constants/connections.ts
+++ src/transaction/constants/connections.ts
@@ -1,3 +1,8 @@
-export const MONGO_CONNECTION =
-  process.env.MONGO_HOST ||
-  'mongodb://root:root@localhost:29017/nest?authSource=admin';
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'root';
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+const MONGO_PORT = process.env.MONGO_PORT || 29017;
+const MONGO_DB = process.env.MONGO_DB || 'nest';
+const MONGO_AUTH_SOURCE = process.env.MONGO_AUTH_SOURCE || 'admin';
+
+export const MONGO_CONNECTION = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=${MONGO_AUTH_SOURCE}`;
